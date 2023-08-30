c99ad58c68cf55c2d8b7f22684746269a3dd835b
YaroslavMatushevych <optima19977@gmail.com>
2023-08-30 10:32:49 +0200

    feature: created site config, tailwind-indicator

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Blog Posts",
+  description: "A list of blog posts and their basic information.",
+  mainNav: [],
+  links: {
+    twitter: "https://twitter.com/YaroslavMatush",
+    github: "https://github.com/YaroslavMatushevych/next.js-pagination-filtering-search-posts",
+  },
+}
