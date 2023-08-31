0f39ef001bcc705d0e8758374cd4259d9371173d
YaroslavMatushevych <optima19977@gmail.com>
2023-08-31 02:38:27 +0200

    fix: ts errors

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -4,6 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next/types";
 import { NextResponse } from 'next/server';
 import { BlogPosts } from '@/types/posts';
 
+//@ts-ignore
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
   //@ts-ignore
   const url = new URL(req.url)
